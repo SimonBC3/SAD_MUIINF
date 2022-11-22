11328137c283e73ea21d7137df08a732e1a14b7e
SimonBC3 <sbrotons3@gmail.com>
2022-11-22 18:48:51 +0100

    Keycloak protects the frontend

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const keycloak = require('./keycloak-config.js').initKeycloak()
 
 app.use(express.json());
+app.use(keycloak.middleware())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,7 +14,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.post('/post', function(req,res) {
+app.post('/post', keycloak.protect('user'), function(req,res) {
     console.log()
     console.log(req.body)
     
