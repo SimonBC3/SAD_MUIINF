6626a164f526bbe61203e33ff058778614ceb29e
SimonBC3 <sbrotons3@gmail.com>
2023-01-26 13:52:11 +0100

    post request now sends a kafka message.

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,8 +1,11 @@
+
 const express = require('express')
 const app = express()
-const port = 3000
-//const keycloak = require('./keycloak-config.js').initKeycloak()
 const kafka = require('./../kafka.js')
+//const keycloak = require('./keycloak-config.js').initKeycloak()
+
+const port = 3000
+var nJob = 0
 
 app.use(express.json());
 //app.use(keycloak.middleware())
@@ -11,27 +14,36 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/job', function(req,res) {
+
+  console.log(req.body)
+    //producer
+    run(nJob, req.body.url, req.body.path, req.body.fileName).catch( error => { return res.status(500).send('Internal error!')})
+    //uuid
+    nJob++
+    //answer to client
+    return res.status(200).send('Petition sent!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.post('/work', function(req,res) {
-    console.log()
-    console.log(req.body)
-    
-    //productor
-    run(req.body.url)
-    //generar uuid = topic
-})
 
-async function run(url){
+//TODO: somewhere else and func imported
+async function run(uuid, url, path, fileName){
   const producer = kafka.producer()
   await producer.connect()
 
   await producer.send({
-    topic: 'myRandomTopicString123',
+    topic: 'users',
     messages: [
-        { value: url },
+        { value: JSON.stringify({ uuid: `${uuid}`, 
+                                  url: `${url}`, 
+                                  path: `${path}`, 
+                                  fileName:`${fileName}`
+                                }) 
+        },
     ],
 })
 }
--- kafka.js
+++ kafka.js
@@ -5,14 +5,4 @@ const kafka = new Kafka({
     brokers: ['localhost:29092','0.0.0.0:29092']
 })
 
-const admin = kafka.admin()
-admin.connect()
-admin.createTopics({
-    waitForLeaders: true,
-    topics: [
-        { topic: 'myRandomTopicString123' },
-    ],
-})
-
-
 module.exports = kafka
