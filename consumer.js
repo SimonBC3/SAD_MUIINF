0d608f3a5b7aed527a1dedb2106042cafbcb0649
SkyRexDEV <skyrexhd@gmail.com>
2023-01-26 19:51:24 +0100

    formatting

--- consumer.js
+++ consumer.js
@@ -1,15 +1,15 @@
-const { Kafka } = require('kafkajs')
+const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
-  clientId: 'my-app',
-  brokers: ['localhost:29092']
-})
+  clientId: "my-app",
+  brokers: ["localhost:29092"],
+});
 
-const consumer = kafka.consumer({ groupId: 'out-group' })
+const consumer = kafka.consumer({ groupId: "out-group" });
 
 const run = async () => {
-  await consumer.connect()
-  await consumer.subscribe({ topic: 'out', fromBeginning: false })
+  await consumer.connect();
+  await consumer.subscribe({ topic: "out", fromBeginning: false });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
@@ -17,9 +17,9 @@ const run = async () => {
         partition,
         offset: message.offset,
         value: message.value.toString(),
-      })
+      });
     },
-  })
-}
+  });
+};
 
-run().catch(console.error)
+run().catch(console.error);
